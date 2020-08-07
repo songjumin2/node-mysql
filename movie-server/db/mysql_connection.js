994388a6f4f5212a11cc7c97ef607b2d28060595
songjumin12 <thd12343@gmail.com>
2020-08-07 16:28:59 +0900

    add

--- movie-server/db/mysql_connection.js
+++ movie-server/db/mysql_connection.js
@@ -1,14 +1,11 @@
 // promise로 개발된, mysql2패키지를 설치하고 로딩
 const mysql = require("mysql2");
 
-// db-config.json 에 저장된 중요 정보를 여기서 셋팅.
-const db_config = require("../config/db-config.json");
-
 const pool = mysql.createPool({
-  host: db_config.MYSQL_HOST,
-  user: db_config.MYSQL_USER,
-  database: db_config.DB_NAME,
-  password: db_config.DB_PASSWD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWD,
   waitForConnections: true,
   connectionLimit: 10,
 });
